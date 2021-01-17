64780e048f9a491680f92fccf5c49eab3034807e
Danny Renato <danny.renato@mydna.life>
2021-01-18 01:30:00 +1100

    Add usage of tennis service

--- GoSportFrontEnd/GoSportWebPage/src/app/app.component.ts
+++ GoSportFrontEnd/GoSportWebPage/src/app/app.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { TennisService } from './tennisService/tennis.service';
+import { TennisMatchDetails } from './tennisService/tennisMatchDetails';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
-  constructor(private tennis:TennisService){
+export class AppComponent implements OnInit {
+  // TODO: Refactor to its own component
+  constructor(
+    private tennisService: TennisService,
+    private route: ActivatedRoute){
 
   }
-  title = 'GoSportWebPage';
 
+  matchId: string;
+  matchDetails$: Observable<TennisMatchDetails>;
 
+  ngOnInit() {
+    this.matchDetails$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        this.matchId = params.get('id');
+        return this.tennisService.getMatchDetails(this.matchId);
+      })
+    );
+  }
 }
